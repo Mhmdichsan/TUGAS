fa2de1bcd49e871d930799324e240658b05722e8
Mhmdichsan <sannn0407@gmail.com>
2022-06-11 17:20:25 +0800

    Pertama

---
+++ po/src/LOnte/kontol.java
@@ -0,0 +1,10 @@
+package LOnte;
+
+public class kontol {
+
+    public static void main(String[] args) {
+        int harga;
+        System.out.println("lo kontol");
+        System.out.println("lo semua ngentot");
+    }
+}
